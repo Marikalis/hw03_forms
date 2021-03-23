aeb9e5a3c10fab01e8a0046056fd1704181fbddc
Мария Лисицкая <lisitskayamari@gmail.com>
2021-03-23 17:22:18 +0000

    nav in base

--- posts/models.py
+++ posts/models.py
@@ -1,35 +1,39 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-DATE_FORMAT = "%d/%m/%Y %H:%M"
+from .settings import DATE_FORMAT
 
 User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name="Текст")
+    text = models.TextField(
+        verbose_name='Текст'
+    )
     pub_date = models.DateTimeField(
-        auto_now_add=True, verbose_name="Дата публикации",
+        auto_now_add=True,
+        verbose_name='Дата публикации',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="posts",
-        verbose_name="Автор",
+        related_name='posts',
+        verbose_name='Автор',
     )
     group = models.ForeignKey(
         'Group',
         on_delete=models.SET_NULL,
-        related_name="posts",
-        verbose_name="Группа",
+        related_name='posts',
+        verbose_name='Группа',
         blank=True,
-        null=True)
+        null=True
+    )
 
     def __str__(self):
-        return (f"{self.text[:20]} | "
-                f"{self.group} | "
-                f"{self.pub_date.strftime(DATE_FORMAT)} | "
-                f"{self.author}")
+        return (f'{self.text[:20]} | '
+                f'{self.group} | '
+                f'{self.pub_date.strftime(DATE_FORMAT)} | '
+                f'{self.author.name.get_full_name}')
 
     class Meta:
         ordering = ('-pub_date',)
@@ -38,9 +42,17 @@ class Post(models.Model):
 
 
 class Group(models.Model):
-    title = models.CharField(verbose_name="Название", max_length=200)
-    slug = models.SlugField(verbose_name="Ссылка", unique=True)
-    description = models.TextField(verbose_name="Описание")
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    slug = models.SlugField(
+        verbose_name='Ссылка',
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
 
     def __str__(self):
         return self.title
