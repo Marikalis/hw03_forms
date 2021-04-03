d5c9c2fcd20546e45827cd90efd6c43f44ea3f4d
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-03 15:58:09 +0100

    test_urls is complete

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,4 +1,5 @@
 from django.test import Client, TestCase
+
 from posts.models import Group, Post, User
 
 
@@ -25,6 +26,29 @@ class URLTests(TestCase):
         self.authorized_client = Client()
         # Авторизуем пользователя
         self.authorized_client.force_login(self.user)
+
+    def test_index_url_exists_at_desired_location(self):
+        """Страница / доступна любому пользователю."""
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_group_url_exists_at_desired_location(self):
+        """Страница /group/test-slug/ доступна любому пользователю."""
+        response = self.guest_client.get('/group/test-slug/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post_url_exists_at_desired_location(self):
+        """Страница /new/ доступна авторизованному пользователю."""
+        response = self.authorized_client.get('/new/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post_url_redirect_anonymous_on_admin_login(self):
+        """Страница /new/ перенаправит анонимного пользователя
+        на страницу логина.
+        """
+        response = self.guest_client.get('/new/', follow=True)
+        self.assertRedirects(
+            response, '/auth/login/?next=/new/')
 
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
