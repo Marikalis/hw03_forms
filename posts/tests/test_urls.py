2ff3f1038d3d5abf207059fc11916e95341fb451
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-03 15:24:23 +0100

    add test_urls_uses_correct_template

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,15 +1,40 @@
 from django.test import Client, TestCase
-from django.urls import reverse
+from posts.models import Group, Post, User
 
 
-class StaticURLTests(TestCase):
+class URLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = Group.objects.create(
+            title='Тестовое название',
+            description='Тестовое описание',
+            slug='test-slug'
+        )
+        cls.post = Post.objects.create(
+            text='a' * 20,
+            author=User.objects.create_user(username='testuser')
+        )
+
     def setUp(self):
-        # Устанавливаем данные для тестирования
-        # Создаём экземпляр клиента. Он неавторизован.
+        # Создаем неавторизованный клиент
         self.guest_client = Client()
+        # Создаем пользователя
+        self.user = User.objects.create_user(username='MarieL')
+        # Создаем второй клиент
+        self.authorized_client = Client()
+        # Авторизуем пользователя
+        self.authorized_client.force_login(self.user)
 
-    def test_homepage(self):
-        # Отправляем запрос через client,
-        # созданный в setUp()
-        response = self.guest_client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        # Шаблоны по адресам
+        templates_url_names = {
+            'index.html': '/',
+            'group.html': '/group/test-slug/',
+            'new_post.html': '/new/',
+        }
+        for template, url in templates_url_names.items():
+            with self.subTest():
+                response = self.authorized_client.get(url)
+                self.assertTemplateUsed(response, template)
