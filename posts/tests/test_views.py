23fd9c023384134610f2caa7eca4e780c0721a21
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-04 14:52:51 +0100

    test_views added

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,29 +1,34 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-from django import forms
 
-from deals.models import Group, Post, User
+from posts.models import Group, Post, User
+
 
 class PagesTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.group = Group.objects.create(
-            title='Тестовое название',
-            description='Тестовое описание',
+        cls.group_with_post = Group.objects.create(
+            title='Группа с постом',
+            description='Описание',
             slug='test-slug'
+        )
+        cls.group_without_post = Group.objects.create(
+            title='Группа без поста',
+            description='Тестовое описание',
+            slug='test-slug-empty'
         )
         cls.post = Post.objects.create(
             text='Тестовый пост',
-            author=User.objects.create_user(username='testuser')
+            author=User.objects.create_user(username='testuser'),
+            group=cls.group_with_post
         )
 
     def setUp(self):
-        # Создаём неавторизованный клиент
-        self.guest_client = Client()
         # Создаём авторизованный клиент
-        self.user = User.objects.create_user(username='StasBasov')
+        self.user = User.objects.create_user(username='Test_lisa')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
 
@@ -32,11 +37,11 @@ class PagesTests(TestCase):
         """URL-адрес использует соответствующий шаблон."""
         # Собираем в словарь пары "имя_html_шаблона: reverse(name)"
         templates_page_names = {
-            'index.html': reverse('posts:index'),
+            'index.html': reverse('index'),
             'group.html': (
-                reverse('posts:group_posts', kwargs={'slug': 'test-slug'})
+                reverse('group_posts', kwargs={'slug': 'test-slug'})
             ),
-            'new_post.html': reverse('posts:new_post'),
+            'new_post.html': reverse('new_post'),
         }
 
         # Проверяем, что при обращении к name
@@ -46,17 +51,31 @@ class PagesTests(TestCase):
                 response = self.authorized_client.get(reverse_name)
                 self.assertTemplateUsed(response, template)
 
-    def test_index_page_show_correct_context(self):
+    def test_index_show_correct_context(self):
         """Шаблон index сформирован с правильным контекстом."""
-        response = self.guest_client.get(reverse('posts:index'))
+        response = self.authorized_client.get(reverse('index'))
+        posts = response.context['posts']
+        expected = Post.objects.all()
+        self.assertEqual(list(posts), list(expected))
+
+    def test_group_show_correct_context(self):
+        """Шаблон group_posts сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('group_posts', kwargs={'slug': 'test-slug'}))
+        group = response.context['group']
+        expected_group = PagesTests.group_with_post
+        posts = response.context['posts']
+        expected_posts = expected_group.posts.all()
+        self.assertEqual(list(posts), list(expected_posts))
+        self.assertEqual(group, expected_group)
+
+    def test_new_post_page_show_correct_context(self):
+        """Шаблон new_post сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('new_post'))
         # Словарь ожидаемых типов полей формы:
         # указываем, объектами какого класса должны быть поля формы
         form_fields = {
-            'title': forms.fields.CharField,
-            # При создании формы поля модели типа TextField
-            # преобразуются в CharField с виджетом forms.Textarea
-            'description': forms.fields.TextField,
-            'slug': forms.fields.SlugField,
+            'group': forms.models.ModelChoiceField,
+            'text': forms.fields.CharField
         }
         # Проверяем, что типы полей формы в словаре context
         # соответствуют ожиданиям
@@ -66,9 +85,18 @@ class PagesTests(TestCase):
                 # Проверяет, что поле формы является экземпляром
                 # указанного класса
                 self.assertIsInstance(form_field, expected)
-        
-        #def test_post_list_page_list_is_1(self):
-        # Удостоверимся, что на страницу со списком заданий передаётся
-        # ожидаемое количество объектов
-        #response = self.authorized_client.get(reverse('posts:task_list'))
-        #self.assertEqual(len(response.context['object_list']), 1)
+
+    def test_new_post_with_group_shown_on_index(self):
+        # Удостоверимся, что если при создании поста указать группу,
+        # то этот пост появляется
+        response = self.authorized_client.get(reverse('index'))
+        post_text = response.context['posts'][0].text
+        expected_text = PagesTests.post.text
+        self.assertEqual(post_text, expected_text)
+
+    def test_new_post_with_group_doesnt_shown_on_other_group(self):
+        # Удостоверимся, что если при создании поста указать группу,
+        # то этот пост не появляется в другой группе
+        response = self.authorized_client.get(reverse('group_posts', kwargs={'slug': 'test-slug-empty'}))
+        posts = response.context['posts']
+        self.assertEqual(len(posts), 0)
