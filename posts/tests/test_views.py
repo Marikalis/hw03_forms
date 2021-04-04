31e6210bcaf6d325d5f281ac1e5933205aeb8e3f
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-04 01:20:53 +0100

    при создании поста указать группу, то этот пост появляется в работе

---
+++ posts/tests/test_views.py
@@ -0,0 +1,74 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from django import forms
+
+from deals.models import Group, Post, User
+
+class PagesTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = Group.objects.create(
+            title='Тестовое название',
+            description='Тестовое описание',
+            slug='test-slug'
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый пост',
+            author=User.objects.create_user(username='testuser')
+        )
+
+    def setUp(self):
+        # Создаём неавторизованный клиент
+        self.guest_client = Client()
+        # Создаём авторизованный клиент
+        self.user = User.objects.create_user(username='StasBasov')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    # Проверяем используемые шаблоны
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        # Собираем в словарь пары "имя_html_шаблона: reverse(name)"
+        templates_page_names = {
+            'index.html': reverse('posts:index'),
+            'group.html': (
+                reverse('posts:group_posts', kwargs={'slug': 'test-slug'})
+            ),
+            'new_post.html': reverse('posts:new_post'),
+        }
+
+        # Проверяем, что при обращении к name
+        # вызывается соответствующий HTML-шаблон
+        for template, reverse_name in templates_page_names.items():
+            with self.subTest(template=template):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
+
+    def test_index_page_show_correct_context(self):
+        """Шаблон index сформирован с правильным контекстом."""
+        response = self.guest_client.get(reverse('posts:index'))
+        # Словарь ожидаемых типов полей формы:
+        # указываем, объектами какого класса должны быть поля формы
+        form_fields = {
+            'title': forms.fields.CharField,
+            # При создании формы поля модели типа TextField
+            # преобразуются в CharField с виджетом forms.Textarea
+            'description': forms.fields.TextField,
+            'slug': forms.fields.SlugField,
+        }
+        # Проверяем, что типы полей формы в словаре context
+        # соответствуют ожиданиям
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context['form'].fields[value]
+                # Проверяет, что поле формы является экземпляром
+                # указанного класса
+                self.assertIsInstance(form_field, expected)
+        
+        #def test_post_list_page_list_is_1(self):
+        # Удостоверимся, что на страницу со списком заданий передаётся
+        # ожидаемое количество объектов
+        #response = self.authorized_client.get(reverse('posts:task_list'))
+        #self.assertEqual(len(response.context['object_list']), 1)
