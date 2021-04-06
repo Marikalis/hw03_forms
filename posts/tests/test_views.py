482f59854ee486bd6e3b5e523921e4640bef14b1
Мария Лисицкая <lisitskayamari@gmail.com>
2021-04-06 17:46:47 +0100

    create class PaginatorViewsTest(TestCase) test_views

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -103,3 +103,28 @@ class PagesTests(TestCase):
         )
         posts = response.context['posts']
         self.assertEqual(len(posts), 0)
+
+
+class PaginatorViewsTest(TestCase):
+    # Здесь создаются фикстуры: клиент и 13 тестовых записей.
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        for index in xrange(12):
+            note = f"запись номер {index} "
+            Post.objects.create(
+                text: note)
+
+    def setUp(self):
+        # Создаем неавторизованный клиент
+        self.guest_client = Client()
+
+    def test_first_page_containse_ten_records(self):
+        response = self.client.get(reverse('index'))
+        # Проверка: количество постов на первой странице равно 10.
+        self.assertEqual(len(response.context.get('page').object_list), 10)
+
+    def test_second_page_containse_three_records(self):
+        # Проверка: на второй странице должно быть три поста.
+        response = self.client.get(reverse('index') + '?page=2')
+        self.assertEqual(len(response.context.get('page').object_list), 3)
